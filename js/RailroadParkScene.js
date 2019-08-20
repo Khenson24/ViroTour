5a1565f912a063b9c21765e7ced66f155bd14564
Kinard Henson <kinardhenson@Kinards-MacBook-Air.local>
2019-08-20 13:53:38 -0500

    message1

---
+++ js/RailroadParkScene.js
@@ -0,0 +1,213 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import { StyleSheet } from 'react-native';
+
+import {
+  ViroButton,
+  ViroARScene,
+  ViroText,
+  ViroConstants,
+  ViroARImageMarker,
+  ViroBox,
+  ViroARTrackingTargets,
+  ViroARPlaneSelector,
+  ViroVideo,
+  ViroNode,
+  ViroAnimations
+} from 'react-viro';
+
+export default class RailroadPark extends Component {
+
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text: "Hold Up",
+      text2: "Hold Up",
+      text3: "Hold Up",
+      text4: "Hold Up",
+      tapButton: false
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+    this._onAnchorFound = this._onAnchorFound.bind(this);
+    this._onClickState = this._onClickState.bind(this);
+    this._animateFinished = this._animateFinished.bind(this);
+  }
+
+  render() {
+    return (
+
+      <ViroARScene onTrackingUpdated={this._onInitialized} anchorDetectionTypes={"Vertical"}>
+        <ViroARImageMarker target={"targetOne"} onAnchorFound={this._onAnchorFound} >
+          <ViroNode>
+            <ViroButton
+              source={require("./res/download.jpeg")}
+              tapSource={require("./res/IMG_0485.jpeg")}
+              position={[0, 0, 0]}
+              rotation={[-90, 90, 0]}
+              height={0.5}
+              width={0.5}
+              onClickState={this._onClickState}
+              transformBehaviors={["billboard"]}
+              visible={true}
+              />
+          </ViroNode>
+        </ViroARImageMarker>
+        <ViroARImageMarker target={"targetTwo"} >
+        <ViroText
+            text={this.state.text3}
+            scale={[.5, .5, .5]}
+            position={[0, 0, 0]}
+            rotation={[-90, 90, 0]}
+            style={styles.helloWorldTextStyle}
+            transformBehaviors={["billboard"]}
+          />
+          <ViroText
+            text={this.state.text2}
+            scale={[.5, .5, .5]}
+            position={[0, 0, 0]}
+            rotation={[-90, 90, 0]}
+            style={styles.helloWorldTextStyle}
+            transformBehaviors={["billboard"]}
+          />
+        </ViroARImageMarker>
+        <ViroARImageMarker target={"targetThree"} >
+          <ViroText
+            text={this.state.text3}
+            scale={[.5, .5, .5]}
+            position={[0, 0, 0]}
+            rotation={[-90, 90, 0]}
+            style={styles.helloWorldTextStyle}
+            transformBehaviors={["billboard"]}
+          />
+        </ViroARImageMarker>
+        <ViroARImageMarker target={"targetFour"} >
+          <ViroText
+            text={this.state.text4}
+            scale={[.5, .5, .5]}
+            position={[0, 0, 0]}
+            rotation={[-90, 90, 0]}
+            style={styles.helloWorldTextStyle}
+            transformBehaviors={["billboard"]}
+          />
+        </ViroARImageMarker>
+
+      </ViroARScene>
+
+      // <ViroARScene onTrackingUpdated={this._onInitialized} >
+      //   <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      //   <ViroText text={this.state.text2} scale={[.5, .5, .5]} position={[0, 1, -4]} style={styles.helloWorldTextStyle} />
+      // </ViroARScene>
+    );
+  }
+
+  _onAnchorFound(state) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        visible: true,
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      this.setState({
+        visible: false,
+      });      
+    }
+  }
+
+  _onClickState(stateValue, position, source)  {
+    if(stateValue == 1) {
+      this.setState({
+        tapButton: true,
+        animation: {name:'tapAnimationUp'},
+        position: [[0,0,0]]
+      });
+    } else if(stateValue == 2) {
+      this.setState({
+        tapButton: false,
+        animation: {name:'tapAnimationDown'},
+        position: [[0,0,0]]
+      });
+    }
+}
+
+  _animateFinished(){
+    this.setState({
+      tapButton: false
+    })
+  }
+
+  _onInitialized(state, reason) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        text: "Welcome to the Innovate Depot Tour! Head through the double doors and walk straight.",
+        text2: "Keep on truckin. If you'd like to see some info on the CEO hover over his placard. When you make it to the large room, hover camera with steps in the center",
+        text3: "This is our CEO's office. Do not disturb. Lots of innovation in progress.",
+        text4: "Welcome to the Atrium :D! Hungry or lonely? You can solve both here. Hover over the Crestline doors for more info otherwise check out the board of pictures to the right of the steps.",
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+    }
+  }
+
+}
+
+// ViroARTrackingTargets.createTargets({
+//   "targetOne": {
+//     source: require('./res/IMG_0485.jpeg'),
+//     orientation: "Up",
+//     physicalWidth: 0.75 // real world width in meters
+//   },
+//   "targetTwo": {
+//     source: require('./res/IMG_0486.jpeg'),
+//     orientation: "Up",
+//     physicalWidth: 0.75
+//   },
+//   "targetThree": {
+//     source: require('./res/IMG_0487.jpeg'),
+//     orientation: "Up",
+//     physicalWidth: 0.75
+//   },
+//   "targetFour": {
+//     source: require('./res/IMG_0488.jpeg'),
+//     orientation: "Up",
+//     physicalWidth: 0.75
+//   }
+// });
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 12,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
+
+ViroAnimations.registerAnimations({
+  scaleButtonUp:{
+    properties: {
+      scaleX: 2,
+      scaleY: 2,
+      scaleZ: 2
+    },
+    easing: "easeineaseout",
+    duration: 2000    
+  },
+  scaleButtonDown:{
+    properties: {
+      scaleX: .75,
+      scaleY: .75,
+      scaleZ: .75
+    },
+    easing: "easeineaseout",
+    duration: 2000    
+  },
+  tapAnimationUp: [["scaleButtonUp"]],
+  tapAnimationDown: [["scaleButtonDown"]]
+});
+
+module.exports = RailroadPark;
